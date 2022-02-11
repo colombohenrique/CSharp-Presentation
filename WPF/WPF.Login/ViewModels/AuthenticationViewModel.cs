b408084784ab195f4fb8a3abb1ce6218b0d10c20
colombohenrique <jhbcolombo@gmail.com>
2022-02-11 01:43:45 -0300

    Adjustments on LoginPage

--- WPF/WPF.Login/ViewModels/AuthenticationViewModel.cs
+++ WPF/WPF.Login/ViewModels/AuthenticationViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Haley.Abstractions;
 using Haley.Models;
+using Haley.WPF.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,8 @@ namespace WPF.Login.ViewModels
 {
     public class AuthenticationViewModel : BaseVM
     {
+        private IDialogService ds;
+
         private object _currentView;
         public object CurrentView
         {
@@ -18,25 +21,39 @@ namespace WPF.Login.ViewModels
             set { SetProp(ref _currentView, value); }
         }
 
+        private string _login;
+        public string Login
+        {
+            get { return _login; }
+            set { SetProp(ref _login, value); }
+        }
+
 
         public ICommand CmdChangeView => new DelegateCommand<object>((o) => CurrentView = o);
-        public ICommand CmdCreateAccount => new DelegateCommand(_createAccount);
 
-        public ICommand CmdSingIn => new DelegateCommand(_singIn);
 
-        public ICommand CmdSendEmail => new DelegateCommand(_sendEmail);
-
-        private void _sendEmail()
-        {
-            //SEND EMAIL RECOVER PASSWORD
-        }
-
-        private void _singIn()
+        public ICommand CmdSingIn => new DelegateCommand<object>(_singIn);
+        private void _singIn(object obj)
         {
             //Sing in.
-            InvokeVMClosed(this, new Haley.Events.FrameClosingEventArgs(true,"Authenticated"));
+
+            var _lgn = Login;
+            if(obj is PlainPasswordBox passwordBox)
+            {
+                var _password = passwordBox.GetPassword();
+                string pass = "test";
+                if (string.IsNullOrWhiteSpace(_password) || !_password.Equals(pass)) 
+                {
+                    ds?.SendToast("Error", "Password doesn't match!", Haley.Enums.NotificationIcon.Error);
+                    return;
+                }
+
+            }
+            InvokeVMClosed(this, new Haley.Events.FrameClosingEventArgs(true, "Authenticated"));
         }
 
+
+        public ICommand CmdCreateAccount => new DelegateCommand(_createAccount);
         private void _createAccount()
         {
             //Create account
@@ -46,9 +63,19 @@ namespace WPF.Login.ViewModels
             CurrentView = Views.ViewEnums.LoginPage;
         }
 
-        public AuthenticationViewModel()
+
+
+        public ICommand CmdSendEmail => new DelegateCommand(_sendEmail);
+        private void _sendEmail()
+        {
+            //SEND EMAIL RECOVER PASSWORD
+        }
+       
+
+        public AuthenticationViewModel(IDialogService dialogService)
         {
             CurrentView = Views.ViewEnums.LoginPage;
+            ds = dialogService;
         }
     }
 }
