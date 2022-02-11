b850ca658714f5d81735a87006b972e9af3fb731
colombohenrique <jhbcolombo@gmail.com>
2022-02-10 23:45:51 -0300

    Added login screen

--- WPF/WPF.Project/WPF.Project/App.xaml.cs
+++ WPF/WPF.Project/WPF.Project/App.xaml.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Haley.Abstractions;
+using Haley.MVVM;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using WPF.Login;
+using WPF.Login.Views;
 using WPF.Login.Controls;
 
 namespace WPF.Project
@@ -15,19 +17,40 @@ namespace WPF.Project
     /// </summary>
     public partial class App : Application
     {
+        private IDialogService _ds;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _ds = ContainerStore.Singleton.DI.Resolve<IDialogService>();
+            base.OnStartup(e);
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-            var _actualMainWindow = new MainWindow();  
+            var _actualMainWindow = new MainWindow();
+
+            int maxTries = 5;
+            int tryCount = 0;
             while (!CredentialHolder.Singleton.IsAuthenticated)
             {
                 var _authWindow = new AuthenticationWindow();
                 _authWindow.ShowDialog();
+                tryCount++;
+                if (tryCount >= maxTries) break;
             }
 
             if (CredentialHolder.Singleton.IsAuthenticated)
             {
                 //SHOW MAIN WINDOW
                 _actualMainWindow.Show();
+            }
+            else
+            {
+                _ds?.Error("Failure","Unable to authenticate! The Application will close.");
+                if (Application.Current.MainWindow != null)
+                {
+                    Application.Current.MainWindow.Close();
+                }
             }
         }
 
